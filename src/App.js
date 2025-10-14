655e3e3bbe6905e36d8b60309f9341a20525d55b
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2025-10-14 16:58:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from "./component/Skills";
 import Archiving from "./component/Archiving.js";
 import Project from "./component/Project.js";
 import Career from "./component/Career.js";
-//whsdjsaknfjsan;d ltlqkf kwsmdk
+//whsdjsaknfjsan;d ltlqkf kwsm
 function App() {
   const scrollToSection = (sectionId) => {
     const element = document.getElementById(sectionId);
