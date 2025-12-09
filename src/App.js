0824de7b7755980cee2f8abeb03a26f0bb560e57
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2025-12-09 09:14:54 +0900

    Fix comment formatting in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from "./component/Skills";
 import Archiving from "./component/Archiving.js";
 import Project from "./component/Project.js";
 import Career from "./component/Career.js";
-//wlfkf ghkdydlkfdlfkrh wldlfkf w
+//wlfkf ghkdydlkfdlfkrh wldlfk
 function App() {
   const scrollToSection = (sectionId) => {
     const element = document.getElementById(sectionId);
