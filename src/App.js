88867325af2f95ad0b973884ae38b4faeedc3121
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2026-06-09 09:46:34 +0900

    Clean up comment in App.js
    Removed unnecessary comment from App.js.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from "./component/Skills";
 import Archiving from "./component/Archiving.js";
 import Project from "./component/Project.js";
 import Career from "./component/Career.js";
-//dnjqksk dnbn ghdmsf ghkdy
+//dnjqksk dnbn ghdmsf ghk
 function App() {
   const scrollToSection = (sectionId) => {
     const element = document.getElementById(sectionId);
