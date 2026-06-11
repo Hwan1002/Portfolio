70c7eb05ba3f51507403255450ec520855e75702
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2026-06-11 09:01:48 +0900

    Clean up code by removing a commented line
    Removed unnecessary comment from App.js.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from "./component/Skills";
 import Archiving from "./component/Archiving.js";
 import Project from "./component/Project.js";
 import Career from "./component/Career.js";
-//dmkasmkdlas w;flflafrj tlgmkfmkf,lflgglf
+//dmkasmkdlas w;flflafrj tlgmkfmkf,lflgg
 function App() {
   const scrollToSection = (sectionId) => {
     const element = document.getElementById(sectionId);
