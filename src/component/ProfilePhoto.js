180a7df7878f247f0e913135f4d3c0bf5cd036b5
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2026-09-09 09:03:49 +0900

    Update ProfilePhoto.js

--- src/component/ProfilePhoto.js
+++ src/component/ProfilePhoto.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import hwan from "../css/img/profile/hwan.jpg";
-//11111111111111111
+//1111111111111111
 const EMOJIS = ["❤️", "❤️", "❤️", "❤️", "❤️", "❤️", "❤️", "❤️"];
 const MESSAGES = [
   "안녕하세요! 👋",
