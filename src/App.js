4985bde0b7315f9f1083052d901719976f26d075
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2026-09-22 16:30:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Career from "./component/Career";
 import Footer from "./component/Footer";
 import ScrollTopButton from "./component/ScrollTopButton";
 import { scrollToSection } from "./utils/scroll";
-//11111111111111111111111111111111111111111
+//1111111111111111111111111111111111111111
 const FULL_TITLE = "장환석 포트폴리오";
 
 function App() {
